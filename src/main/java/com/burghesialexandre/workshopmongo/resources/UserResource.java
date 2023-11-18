fe87944371c2fd389bdc494d30464c55c5f4142a
Alexandre Burghesi <alexandreburghesi@hotmail.com>
2023-11-17 22:53:45 -0300

    342. Inserindo Usuario com POST

--- src/main/java/com/burghesialexandre/workshopmongo/resources/UserResource.java
+++ src/main/java/com/burghesialexandre/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.burghesialexandre.workshopmongo.resources;
 
 import java.lang.reflect.Array;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -8,14 +9,12 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.burghesialexandre.workshopmongo.domain.User;
 import com.burghesialexandre.workshopmongo.dto.UserDTO;
 import com.burghesialexandre.workshopmongo.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -39,5 +38,12 @@ public class UserResource {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
+        @RequestMapping(method = RequestMethod.POST)
+        public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+            User obj = service.fromDTO(objDto);
+            obj = service.insert(obj);
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+            return ResponseEntity.created(uri).build();
+        }
 
 }
--- src/main/java/com/burghesialexandre/workshopmongo/services/UserService.java
+++ src/main/java/com/burghesialexandre/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.burghesialexandre.workshopmongo.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.burghesialexandre.workshopmongo.dto.UserDTO;
 import com.burghesialexandre.workshopmongo.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,11 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+    public User insert(User obj) {
+        return repo.insert(obj);
     }
 }
